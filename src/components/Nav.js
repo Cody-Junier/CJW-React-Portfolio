f751742a53e3b91fbb52a95e5c98fe358869d1b2
cody-junier <codyjunier@gmail.com>
2022-05-25 16:28:16 -0700

    Added meta info, updated resume

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,10 +2,10 @@ import React from "react";
 export default function Nav(props){
     return(
         <div id="nav">
-            <a href="#" id="about" className={props.currentCategory === "About" ? 'navActive' : 'navInactive'} onClick={() =>props.setCurrentCategory("About")}>About</a>
-            <a href="#" id="contact" className={props.currentCategory === "Contact" ? 'navActive' : 'navInactive'} onClick={() =>props.setCurrentCategory("Contact")}>Contact</a>
-            <a href="#" id="projects" className={props.currentCategory === "Projects" ? 'navActive' : 'navInactive'} onClick={() =>props.setCurrentCategory("Projects")}>Projects</a>
-            <a href="./CodyJunierResume.pdf" id="resume" className={props.currentCategory === "Resume" ? 'navActive' : 'navInactive'}target="_blank" >Resume</a>
+            <a href="About_Me" id="about" className={props.currentCategory === "About" ? 'navActive' : 'navInactive'} onClick={() =>props.setCurrentCategory("About")}>About</a>
+            <a href="Contact_Me" id="contact" className={props.currentCategory === "Contact" ? 'navActive' : 'navInactive'} onClick={() =>props.setCurrentCategory("Contact")}>Contact</a>
+            <a href="Portfolio" id="projects" className={props.currentCategory === "Projects" ? 'navActive' : 'navInactive'} onClick={() =>props.setCurrentCategory("Projects")}>Projects</a>
+            <a href="./Resume.pdf" id="resume" className={props.currentCategory === "Resume" ? 'navActive' : 'navInactive'}target="_blank" >Resume</a>
         </div>
     )
 }
